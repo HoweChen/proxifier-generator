ab0635d6fa9c92b221db16b6e85636ee80c1baa8
SkyFather <cyh278889789@gmail.com>
2017-08-05 03:02:52 +0800

    update banAD.py

--- banAD.py
+++ banAD.py
@@ -8,15 +8,19 @@ argv = ['outbound_block_list', 'bypass_list', 'proxy_list']
 
 
 def outbound_block_list(file_washed):
+
+    # argc
     outbound_block_list_clean = open(
         file_location + 'outbound_block_list.txt', mode='w').close()
     outbound_block_list_append = open(
         file_location + 'outbound_block_list.txt', mode='a')
     sub_outbound_block_list = []
 
+    # start and end position of keyword
     start = file_washed.index('[outbound_block_list]\n')
     end = file_washed.index('[bypass_list]\n')
 
+    # main part of function
     for line in file_washed[start + 1:end]:
         if line != '\n':
             sub_outbound_block_list.append(line)
@@ -28,14 +32,18 @@ def outbound_block_list(file_washed):
 
 
 def bypass_list(file_washed):
+
+    # argc
     bypass_list_clean = open(
         file_location + 'bypass_list.txt', mode='w').close()
     bypass_list_append = open(file_location + 'bypass_list.txt', mode='a')
     sub_bypass_list = []
 
+    # start and end position of keyword
     start = file_washed.index('[bypass_list]\n')
     end = file_washed.index('[proxy_list]\n')
 
+    # main part of function
     for line in file_washed[start + 1:end]:
         if line != '\n':
             sub_bypass_list.append(line)
@@ -64,7 +72,7 @@ def edit_file(file_combine):
 def download_file(file_combine):
     raw_file = rq.get(
         "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/banAD.acl")
-    # print(raw_file.text)
+
     with open(file_combine, mode='w') as f_write:
         f_write.write(raw_file.text)
         f_write.close()
